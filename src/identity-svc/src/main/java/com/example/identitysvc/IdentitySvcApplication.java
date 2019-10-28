18dfc3c1b47d69bc51984f73d0a3b2a8c954969d
Michael McDermott <michael.mcdermott@jbhunt.com>
2019-10-28 08:43:18 -0500

    Added stub for simple web service.

---
+++ src/identity-svc/src/main/java/com/example/identitysvc/IdentitySvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.identitysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentitySvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentitySvcApplication.class, args);
+	}
+
+}
